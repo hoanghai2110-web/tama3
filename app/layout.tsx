0aeca23404023df7f3b81a957a0a482327cb61b5
hoanghai2110-web <haitricker12@gmail.com>
2025-02-17 22:17:40 +0700

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,29 @@ import { ThemeProvider } from "@/components/custom/theme-provider";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://gemini.vercel.ai"),
-  title: "Next.js Gemini Chatbot",
-  description: "Next.js chatbot template using the AI SDK and Gemini.",
+  title: "Tama - Your AI Assistant",
+  description: "Tama is your witty, confident, and all-knowing AI assistant, ready to assist with anything you need.",
+  openGraph: {
+    images: [
+      {
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCc8Oka5KG7mzlkpeRZE8nVR_KjK4_mMg0NQ&s",
+        width: 800,
+        height: 600,
+        alt: "Tama AI Assistant",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: [
+      {
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCc8Oka5KG7mzlkpeRZE8nVR_KjK4_mMg0NQ&s",
+        width: 800,
+        height: 600,
+        alt: "Tama AI Assistant",
+      },
+    ],
+  },
 };
 
 export default async function RootLayout({
