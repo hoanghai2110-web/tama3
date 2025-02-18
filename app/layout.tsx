b5f836c11faf578b27ea5857d40c7eb5f8507768
hoanghai2110-web <haitricker12@gmail.com>
2025-02-18 21:36:06 +0700

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,11 @@
 "use client"; // Đánh dấu file là client component
 
-import { Metadata } from "next";
 import { Toaster } from "sonner";
 import { Navbar } from "@/components/custom/navbar";
 import { ThemeProvider } from "@/components/custom/theme-provider";
 import { useEffect, useState } from "react";
+import { metadata } from "@/app/metadata"; // Import metadata từ file riêng
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "Tama - Your AI Assistant",
-  description:
-    "Tama is your witty, confident, and all-knowing AI assistant, ready to assist with anything you need.",
-  openGraph: {
-    images: [
-      {
-        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCc8Oka5KG7mzlkpeRZE8nVR_KjK4_mMg0NQ&s",
-        width: 800,
-        height: 600,
-        alt: "Tama AI Assistant",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    images: [
-      {
-        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCc8Oka5KG7mzlkpeRZE8nVR_KjK4_mMg0NQ&s",
-        width: 800,
-        height: 600,
-        alt: "Tama AI Assistant",
-      },
-    ],
-  },
-};
 
 export default async function RootLayout({
   children,
