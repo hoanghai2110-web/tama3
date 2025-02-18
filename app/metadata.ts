df0607f6f664d868b94b205172561f45488a049d
hoanghai2110-web <haitricker12@gmail.com>
2025-02-18 21:36:37 +0700

    metadata.ts

---
+++ app/metadata.ts
@@ -0,0 +1,27 @@
+// app/metadata.ts
+export const metadata = {
+  title: "Tama - Your AI Assistant",
+  description:
+    "Tama is your witty, confident, and all-knowing AI assistant, ready to assist with anything you need.",
+  openGraph: {
+    images: [
+      {
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCc8Oka5KG7mzlkpeRZE8nVR_KjK4_mMg0NQ&s",
+        width: 800,
+        height: 600,
+        alt: "Tama AI Assistant",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: [
+      {
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCc8Oka5KG7mzlkpeRZE8nVR_KjK4_mMg0NQ&s",
+        width: 800,
+        height: 600,
+        alt: "Tama AI Assistant",
+      },
+    ],
+  },
+};
