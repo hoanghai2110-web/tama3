0391837cc7f5e46168b9905c87879b7f34e5b577
hoanghai2110-web <haitricker12@gmail.com>
2025-02-18 21:51:54 +0700

    navbar.tsx

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -40,7 +40,7 @@ export const Navbar = async () => {
           <DropdownMenu>  
              <DropdownMenuTrigger asChild>
               <Button className="py-1.5 px-2 h-fit font-normal" variant="secondary">
-                <MessageIcon className="h-5 w-5" /> {/* Thay thế email bằng icon */}
+                <MessageIcon/> {/* Thay thế email bằng icon */}
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">  
