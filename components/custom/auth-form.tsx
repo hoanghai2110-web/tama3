4929abd6fb13b9d1b50111891d32dff5200a0f19
hoanghai2110-web <haitricker12@gmail.com>
2025-02-18 22:26:40 +0700

    auth-form.tsx

--- components/custom/auth-form.tsx
+++ components/custom/auth-form.tsx
@@ -25,7 +25,7 @@ export function AuthForm({
           name="email"
           className="bg-muted text-md md:text-sm border-none"
           type="email"
-          placeholder="hoanghai@gmail.com"
+          placeholder="hai@mail.com"
           autoComplete="email"
           required
           defaultValue={defaultEmail}
