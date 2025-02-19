4cc28c2dcb154ad5da80fde6aa11c7253164e282
hoanghai2110-web <haitricker12@gmail.com>
2025-02-19 23:08:26 +0700

    message.tsx

--- components/custom/message.tsx
+++ components/custom/message.tsx
@@ -42,12 +42,12 @@ export const Message = ({
       <div className="flex flex-col gap-3 w-full">
         {content && typeof content === "string" && (
           <div
-  className={`flex flex-col px-4 py-2 gap-2 rounded-2xl max-w-[80%] break-words text-left leading-relaxed ${
+  className={`flex flex-col px-4 py-2 gap-2 rounded-2xl max-w-[100%] break-words text-left leading-relaxed ${
     role === "user"
       ? "text-white self-end"
       : "text-zinc-800 dark:text-zinc-300"
   }`}
->
+
 
             <ReactMarkdown
               remarkPlugins={[remarkGfm]}
