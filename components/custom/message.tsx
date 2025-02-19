68df95738e5a9631facc223c1faf091be0cdc18f
hoanghai2110-web <haitricker12@gmail.com>
2025-02-19 21:37:26 +0700

    message.tsx

--- components/custom/message.tsx
+++ components/custom/message.tsx
@@ -5,7 +5,7 @@ import { Attachment, ToolInvocation } from "ai";
 import { motion } from "framer-motion";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { twilight } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { github } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import remarkGfm from "remark-gfm";
 
 const typingVariants = {
@@ -33,7 +33,7 @@ export const Message = ({
 }) => {
   const renderCodeBlock = (code: string, language: string) => {
     return (
-      <SyntaxHighlighter language={language} style={twilight}>
+      <SyntaxHighlighter language={language} style={github}>
         {code}
       </SyntaxHighlighter>
     );
