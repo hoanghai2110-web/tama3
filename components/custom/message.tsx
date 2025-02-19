d8560d708886c0262d946373ecff31a823faedeb
hoanghai2110-web <haitricker12@gmail.com>
2025-02-19 19:04:32 +0700

    message.tsx

--- components/custom/message.tsx
+++ components/custom/message.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { ReactNode } from "react";
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism/material-dark';
+import materialDark from 'react-syntax-highlighter/dist/esm/styles/prism/material-dark';
 import remarkGfm from 'remark-gfm';
 
 
