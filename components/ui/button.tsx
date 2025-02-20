fa857385b1765c0c7b2fe696678997fd6f34e357
hoanghai2110-web <haitricker12@gmail.com>
2025-02-20 19:15:55 +0700

    button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,19 +4,18 @@ import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
-const buttonVariants = cva( 
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50",
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus:ring-0 focus-visible:ring-0 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "bg-background hover:bg-accent hover:text-accent-foreground", // Bỏ hover:border
+        outline: "bg-background hover:bg-accent hover:text-accent-foreground focus-visible:ring-0 focus-visible:outline-none",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-accent hover:text-accent-foreground focus-visible:ring-0 focus-visible:outline-none",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
@@ -31,7 +30,8 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
+
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
