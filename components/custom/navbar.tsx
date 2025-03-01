7da9efc16b5ea4e36dcc38cec921ccaacebc8501
hoanghai2110-web <haitricker12@gmail.com>
2025-03-01 19:27:18 +0700

    navbar.tsx

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
 import Link from "next/link";
 
 import { auth, signOut } from "@/app/(auth)/auth";
 
 import { History } from "./history";
-import { SlashIcon, MessageIcon, UserIcon } from "./icons";
 import { ThemeToggle } from "./theme-toggle";
 import { Button } from "../ui/button";
 import {
@@ -27,7 +25,26 @@ export const Navbar = async () => {
             className="py-1.5 px-2 h-fit font-normal text-white bg-[#007AFF] hover:bg-[#0056b3] transition-colors duration-200 rounded-md"
           >
             <Link href="/get-app" target="_blank">
-              Get App
+              <svg
+                width="80"
+                height="32"
+                viewBox="0 0 80 32"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect width="80" height="32" rx="16" fill="#007AFF" />
+                <text
+                  x="50%"
+                  y="50%"
+                  dominant-baseline="middle"
+                  text-anchor="middle"
+                  font-size="14"
+                  font-family="Arial, sans-serif"
+                  fill="white"
+                >
+                  Get App
+                </text>
+              </svg>
             </Link>
           </Button>
         </div>
@@ -36,11 +53,27 @@ export const Navbar = async () => {
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button className="py-1.5 px-2 h-fit font-normal">
-                <UserIcon size={28} />
+                <svg
+                  width="28"
+                  height="28"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="text-gray-800"
+                >
+                  <path
+                    d="M12 12C14.7614 12 17 9.76142 17 7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7C7 9.76142 9.23858 12 12 12Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M12 14.5C6.20101 14.5 1.5 19.201 1.5 25H22.5C22.5 19.201 17.799 14.5 12 14.5Z"
+                    fill="currentColor"
+                  />
+                </svg>
               </Button>
             </DropdownMenuTrigger>
 
-            <DropdownMenuContent align="end" className="border-none">
+            <DropdownMenuContent align="end" className="border-none bg-white/40 backdrop-blur-xl">
               <DropdownMenuItem>
                 <ThemeToggle />
               </DropdownMenuItem>
