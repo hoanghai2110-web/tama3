72fde954b01c65030d0fd386525d8b8c0be85442
hoanghai2110-web <haitricker12@gmail.com>
2025-03-02 10:11:24 +0700

    navbar.tsx

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -1,16 +1,18 @@
-import Image from "next/image";  
-import Link from "next/link";  
-import { auth, signOut } from "@/app/(auth)/auth";  
-import { History } from "./history";  
-import { SlashIcon, MessageIcon, UserIcon } from "./icons";  
-import { ThemeToggle } from "./theme-toggle";  
-import { Button } from "../ui/button";  
-import {  
-  DropdownMenu,  
-  DropdownMenuContent,  
-  DropdownMenuItem,  
-  DropdownMenuTrigger,  
-} from "../ui/dropdown-menu";  
+import Image from "next/image";
+import Link from "next/link";
+
+import { auth, signOut } from "@/app/(auth)/auth";
+
+import { History } from "./history";
+import { SlashIcon, MessageIcon, UserIcon } from "./icons";
+import { ThemeToggle } from "./theme-toggle";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 // SVG component cho icon điện thoại
 const PhoneIcon = () => (
@@ -20,32 +22,30 @@ const PhoneIcon = () => (
   </svg>
 );
 
-export const Navbar = async () => {  
-  let session = await auth();  
+export const Navbar = async () => {
+  let session = await auth();
 
-  return (  
-    <>  
-      <div className="bg-background absolute top-0 left-0 w-dvw py-2 px-3 justify-between flex flex-row items-center z-30">  
-        <div className="flex flex-row gap-3 items-center">  
-          <History user={session?.user} />  
-          {/* Đã bỏ logo và text TamaAI */}
-        </div>  
+  return (
+    <>
+      <div className="bg-background absolute top-0 left-0 w-dvw py-2 px-3 justify-between flex flex-row items-center z-30">
+        <div className="flex flex-row gap-3 items-center">
+          <History user={session?.user} />
+        </div>
 
         <div className="flex items-center gap-3">
-          {/* Thêm nút GetApp */}
-          <Button 
-            variant="outline" 
+          <Button
+            variant="outline"
             className="py-1.5 px-2 h-fit font-normal border-black bg-transparent hover:bg-gray-100"
           >
             <PhoneIcon />
             <span className="ml-1">GetApp</span>
           </Button>
 
-          {session ? (  
+          {session ? (
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button className="py-1.5 px-2 h-fit font-normal" variant="secondary">
-                  <UserIcon/>
+                  <UserIcon />
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end" className="border-none">
@@ -72,13 +72,13 @@ export const Navbar = async () => {
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
-          ) : (  
-            <Button className="py-1.5 px-2 h-fit font-normal text-white" asChild>  
-              <Link href="/login">Login</Link>  
-            </Button>  
-          )}  
+          ) : (
+            <Button className="py-1.5 px-2 h-fit font-normal text-white" asChild>
+              <Link href="/login">Login</Link>
+            </Button>
+          )}
         </div>
-      </div>  
-    </>  
-  );  
+      </div>
+    </>
+  );
 };
