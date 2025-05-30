de41a506699545eaa26e0365fd3c765d780f9e24
hoanghai2110-web <haitricker12@gmail.com>
2025-05-30 19:26:40 +0700

    index.ts

--- ai/index.ts
+++ ai/index.ts
@@ -4,11 +4,11 @@ import { experimental_wrapLanguageModel as wrapLanguageModel } from "ai";
 import { customMiddleware } from "./custom-middleware";
 
 export const geminiProModel = wrapLanguageModel({
-  model: google("gemini-2.5-flash-preview-05-20"),
+  model: google("gemini-2.5-flash-lite"),
   middleware: customMiddleware,
 });
 
 export const geminiFlashModel = wrapLanguageModel({
-  model: google("gemini-2.0-flash-lite-preview-02-05"),
+  model: google("gemini-2.0-flash"),
   middleware: customMiddleware,
 });
